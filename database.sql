be3dbd527a213cd10c4b02eac2701376203a7d27
Daniel Geymayer <dag0310@gmail.com>
2023-12-27 02:32:16 +0100

    Change UUID column types from VARCHAR(100) to actual UUID datatype
    SQL STATEMENTS:
    
    DROP INDEX idx_receiver_timestamp;
    ALTER TABLE message ALTER COLUMN sender TYPE UUID USING sender::UUID;
    ALTER TABLE message ALTER COLUMN receiver TYPE UUID USING receiver::UUID;
    ALTER TABLE push_subscription ALTER COLUMN receiver TYPE UUID USING receiver::UUID;

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS message;
 
 CREATE TABLE message (
-  sender varchar(100) NOT NULL,
-  receiver varchar(100) NOT NULL,
+  sender uuid NOT NULL,
+  receiver uuid NOT NULL,
   payload varchar(1000000) NOT NULL,
   timestamp bigint NOT NULL,
   PRIMARY KEY(sender, timestamp)
@@ -13,7 +13,7 @@ CREATE INDEX idx_receiver_timestamp ON message(receiver, timestamp);
 DROP TABLE IF EXISTS push_subscription;
 
 CREATE TABLE push_subscription (
-  receiver varchar(100) NOT NULL,
+  receiver uuid NOT NULL,
   endpoint varchar(1024) NOT NULL,
   PRIMARY KEY(receiver, endpoint)
 );
